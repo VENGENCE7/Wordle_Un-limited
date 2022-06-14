62a73650eb042ec393f21efb4048a25baa309118
Bhavish Anand <VENGENCE@localhost.localdomain>
2022-06-14 17:10:42 +0530

    chart data fixed

--- src/Modal/StatsChart.jsx
+++ src/Modal/StatsChart.jsx
@@ -8,19 +8,31 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 export function StatsChart() {
   const { allstats } = useContext(ModalContext);
 
+  const one = allstats.stat_attemptstats.one
+    ? allstats.stat_attemptstats.one
+    : 0;
+  const two = allstats.stat_attemptstats.two
+    ? allstats.stat_attemptstats.two
+    : 0;
+  const three = allstats.stat_attemptstats.three
+    ? allstats.stat_attemptstats.three
+    : 0;
+  const four = allstats.stat_attemptstats.four
+    ? allstats.stat_attemptstats.four
+    : 0;
+  const five = allstats.stat_attemptstats.five
+    ? allstats.stat_attemptstats.five
+    : 0;
+  const six = allstats.stat_attemptstats.six
+    ? allstats.stat_attemptstats.six
+    : 0;
+
   const data = {
     labels: ["ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX"],
     datasets: [
       {
         label: "Attempt STATS",
-        data: [
-          allstats.stat_attemptstats.one,
-          allstats.stat_attemptstats.two,
-          allstats.stat_attemptstats.three,
-          allstats.stat_attemptstats.four,
-          allstats.stat_attemptstats.five,
-          allstats.stat_attemptstats.six,
-        ],
+        data: [one, two, three, four, five, six],
         backgroundColor: [
           "rgba(255,255,51, 0.75)",
           "rgba(0, 191, 255, 0.75)",
@@ -44,23 +56,20 @@ export function StatsChart() {
 
   const options = {
     responsive: [true],
-    maintainAspectRatio: [true],
     animation: [
       {
         easing: "easeInOutCubic",
       },
     ],
-    plugins: [
-      {
-        legend: {
-          labels: {
-            color: "#f0f8ff",
-            font: 16,
-            usePointStyle: true,
-          },
+    plugins: {
+      legend: {
+        labels: {
+          color: "#f0f8ff",
+          font: 16,
+          usePointStyle: true,
         },
       },
-    ],
+    },
   };
 
   return (
